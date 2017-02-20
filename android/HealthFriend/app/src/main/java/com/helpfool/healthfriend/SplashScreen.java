0a5381e676878c93813c660a9bfbca8506f8c949
David Phan <dphan015@ucr.edu>
2017-02-20 14:35:02 -0800

    splash screen

---
+++ android/HealthFriend/app/src/main/java/com/helpfool/healthfriend/SplashScreen.java
@@ -0,0 +1,39 @@
+package com.helpfool.healthfriend;
+
+/**
+ * Created by dphan on 2/20/17.
+ */
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        setContentView(R.layout.activity_splash);
+        //thread for splash screen running
+        Thread logoTimer = new Thread() {
+            public void run() {
+                try {
+                    sleep(2000);
+                } catch (InterruptedException e) {
+                    Log.d("Exception", "Exception" + e);
+                } finally {
+                    startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                }
+                finish();
+            }
+        };
+        logoTimer.start();
+    }
+
+}
