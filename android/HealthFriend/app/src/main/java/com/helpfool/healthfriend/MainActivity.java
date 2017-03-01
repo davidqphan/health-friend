c3d55b54b72c03afeacfcc8ae9aba9598eeaf895
Joel Gomez <joel.gomez@email.ucr.edu>
2017-02-28 21:21:24 -0800

    added settings action to home

--- android/HealthFriend/app/src/main/java/com/helpfool/healthfriend/MainActivity.java
+++ android/HealthFriend/app/src/main/java/com/helpfool/healthfriend/MainActivity.java
@@ -2,9 +2,14 @@ package com.helpfool.healthfriend;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
@@ -23,5 +28,24 @@ public class MainActivity extends AppCompatActivity {
         Intent intro_intent = new Intent(this, IntroActivity.class);
         startActivity(intro_intent);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.home_menu, menu);
+        Drawable drawable = getResources().getDrawable(R.drawable.ic_settings_black_48dp);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+                return true;
+        }
+        return false;
     }
 }
